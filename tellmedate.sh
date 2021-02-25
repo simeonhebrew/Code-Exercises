7a8b7b14e5babf153b9f7e9400efe712b6059ee8
Simeon Hebrew <68329457+simeonhebrew@users.noreply.github.com>
2021-02-25 18:19:55 +0300

    Question 19 Solution

---
+++ tellmedate.sh
@@ -0,0 +1,13 @@
+echo "We are in the year $(date)"
+echo "It is this time: $(date "+%T")"
+echo "What's your name?"
+read varname
+Hour=$(date "+%H")
+
+if [ $Hour -lt 12 ]; then 
+  echo "Goodmorning $varname! it is now $(date "+%T") on this lovely day of $(date "+%F")"
+elif [ $Hour -lt 18 ]; then
+  echo "Goodafternoon $varname! it is now $(date "+%T") on this lovely day of $(date "+%F")"
+else 
+  echo "Goodevening $varname! it is now $(date "+%T") on this lovely day of $(date "+%F")"
+fi
